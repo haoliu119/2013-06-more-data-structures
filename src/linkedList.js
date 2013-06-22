b9b1b0a01ba83d5d4602b2117e2dc571d6d11280
Hack Reactor Students <github@hackreactor.com>
2013-06-22 11:31:41 -0700

    Finished refactoring linkedList to doubly linked list; haven't tested yet

--- src/linkedList.js
+++ src/linkedList.js
@@ -1,32 +1,77 @@
-// Note: don't use an array to do this.
 var makeLinkedList = function(){
   var newLinkedList = {};
   newLinkedList.head = null;
   newLinkedList.tail = null;
 
-    // .addToHead method ++++++++++
+  // .addToHead method ++++++++++
+  // var newHead = call makeNode with value
+  // if this.head is null,
+  //    point this.head to newHead,
+  //    also point this.tail to newHead
+  // else
+  //    point this.head.previous to newHead
+  //    newHead.next points to this.head
+  //    repoint head to newHead
 
-
-  newLinkedList.addToTail = function(value){
-    var newNode = makeNode(value);
-    if(!newLinkedList.head){
-      newLinkedList.head = newNode;
+  newLinkedList.addToHead = function(value){
+    var newHead = makeNode(value);
+    if (!this.head){
+      this.head = this.tail = newHead;
+    } else {
+      this.head.previous = newHead;
+      newHead.next = this.head;
+      this.head = newHead;
     }
-    if(newLinkedList.tail){
-      newLinkedList.tail.next = newNode;
-    }
-    newLinkedList.tail = newNode;
   };
 
+  // refactor addToTail: make .previous point to previous node
+  // var newTail = call makeNode with value
+  // if this.tail is null,
+  //    point this.tail to newTail
+  //    point this.head to newTail
+  // else
+  //    point this.tail.next to newTail
+  //    newTail.previous to this.tail
+  //    repoint tail to newTail
+
+  newLinkedList.addtoTail = function(value){
+    var newTail = makeNode(value);
+    if(!this.tail){
+      this.head = this.tail = newTail;
+    } else {
+      this.tail.next = newTail;
+      newTail.previous = this.tail;
+      this.tail = newTail;
+    }
+  };
+
+  // refactor .removeHead: disconnect removed node from the new head
+  // var tempHead = this.head;
+  // repoint head to tempHead.next
+  // point this.head.previous to null
+  // return tempHead.value
+
   newLinkedList.removeHead = function(){
-    var tempHead = newLinkedList.head.value;
-    newLinkedList.head = newLinkedList.head.next;
-    return tempHead;
+    var tempHead = this.head;
+    this.head = tempHead.next;
+    this.head.previous = null;
+    return tempHead.value;
   };
 
   // .removeTail method ++++++++++
+    // save current tail node into a tempTail
+    // repoint tail to tempTail.previous
+    // this.tail.next = null;
+    // return tempTail.value
 
+    newLinkedList.removeTail = function(){
+      var tempTail = this.tail;
+      this.tail = tempTail.previous;
+      this.tail.next = null;
+      return tempTail.value;
+    };
 
+  // contains should work with doubly linked list as it's coded
   newLinkedList.contains = function(value){
     var recurseContain = function(node){
       return (node.value === value) || !!(node.next && recurseContain(node.next));
@@ -54,8 +99,7 @@ var makeNode = function(value){
   var newNode = {};
   newNode.value = value;
   newNode.next = null;
-  // need new property .previous ++++++++++
-
+  newNode.previous = null;
 
   newNode.removeNextNode = function(){
     newNode.next = null; // what if we use this.next?
