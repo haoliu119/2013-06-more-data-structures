0ad973b19b91bdf8ca9b343df907cf6c4d2908e3
Hao Liu <haoliu119@gmail.com>
2013-06-23 15:32:17 -0700

    .insert implemented and works correctly

--- spec/binarySearchTreeSpec.js
+++ spec/binarySearchTreeSpec.js
@@ -1,5 +1,16 @@
 describe("binarySearchTree", function() {
   var binarySearchTree;
+  var randomNumArray = function(index, limit){
+    limit = limit || index;
+    nums = [];
+    for(var i = 0; i < index; i++){
+      nums.push(Math.floor(Math.random()*limit));
+    }
+    return nums;
+  };
+  var shuffledNumArray = function(index, limit){
+    return _.shuffle(randomNumArray(index, limit));
+  };
 
   beforeEach(function() {
     binarySearchTree = new makeBinarySearchTree();
@@ -10,5 +21,17 @@ describe("binarySearchTree", function() {
     expect(binarySearchTree.contains).toEqual(jasmine.any(Function));
     expect(binarySearchTree.depthFirstLog).toEqual(jasmine.any(Function));
   });
+
+  it("should insert values correctly", function() {
+    var insertArray = shuffledNumArray(10);
+    console.log(insertArray);
+    _.each(insertArray, function(number){
+      binarySearchTree.insert(number);
+    });
+    console.log(binarySearchTree);
+    expect(binarySearchTree.insert).toEqual(jasmine.any(Function));
+  });
+
+
   // add more tests here to test the functionality of binarySearchTree
 });
--- src/binarySearchTree.js
+++ src/binarySearchTree.js
@@ -1,12 +1,20 @@
 var makeBinarySearchTree = function(value){
-	this.parent = null;
+	// this.parent = null;
 	this.left = null;
 	this.right = null;
 	this.value = value || null;
 };
 
 makeBinarySearchTree.prototype.insert = function(value){
-
+	this.value = this.value || value;
+	if (this.value !== value){
+		var direction = (value < this.value)? "left" : "right";
+		if (!this[direction]){
+			this[direction] = new makeBinarySearchTree(value);	
+		} else {
+			this[direction].insert(value);
+		}
+	}
 };
 
 makeBinarySearchTree.prototype.contains = function(value){
