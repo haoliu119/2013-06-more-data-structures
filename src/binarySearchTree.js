7e74406c28160d1015701a44d7f5fe48984c6df4
Hao Liu <haoliu119@gmail.com>
2013-06-23 12:00:29 -0700

    Added .left .right properties to binary tree maker funciton, added .insert .contains .depthFirstLog methods to prototype

--- src/binarySearchTree.js
+++ src/binarySearchTree.js
@@ -1,2 +1,18 @@
-var makeBinarySearchTree = function(){
+var makeBinarySearchTree = function(value){
+	this.parent = null;
+	this.left = null;
+	this.right = null;
+	this.value = value || null;
+};
+
+makeBinarySearchTree.prototype.insert = function(value){
+
+};
+
+makeBinarySearchTree.prototype.contains = function(value){
+
+};
+
+makeBinarySearchTree.prototype.depthFirstLog = function(value){
+
 };
